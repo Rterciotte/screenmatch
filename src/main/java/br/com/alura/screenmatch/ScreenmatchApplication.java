559dc25bcbcfca80d2ab027ecc46b62866fdabd2
Rogério Terciotte <rterciotte@gmail.com>
2025-12-14 17:28:05 -0300

    Adição de classes de dados para séries e para temporadas.

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,16 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.models.DadosEpisodio;
 import br.com.alura.screenmatch.models.DadosSerie;
+import br.com.alura.screenmatch.models.DadosTemporada;
 import br.com.alura.screenmatch.services.ConsumoAPI;
 import br.com.alura.screenmatch.services.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -20,7 +25,19 @@ public class ScreenmatchApplication implements CommandLineRunner {
         var json = consumoAPI.obterDados("http://www.omdbapi.com/?t=MacGyver&apikey=77bc2dca");
         System.out.println(json);
         ConverteDados conversor = new ConverteDados();
-        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-        System.out.println(dados);
+        DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dadosSerie);
+        json = consumoAPI.obterDados("http://www.omdbapi.com/?t=MacGyver&season=2&episode=3&apikey=77bc2dca");
+        DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+        System.out.println(dadosEpisodio);
+
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+        for (int i = 1; i <= dadosSerie.totalTemporadas(); i++){
+            json = consumoAPI.obterDados("http://www.omdbapi.com/?t=MacGyver&season=" + i + "&apikey=77bc2dca");
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+        temporadas.forEach(System.out::println);
     }
 }
